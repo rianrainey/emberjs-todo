d951c296b2c9e52a0aa5fdee3b6c35bad020a7a2
Rian Rainey <rian@rianrainey.com>
2013-12-20 18:39:12 -0500

    showing active todos.

--- js/router.js
+++ js/router.js
@@ -1,5 +1,8 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    // Additional child routes
+    this.route('active');
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
@@ -7,3 +10,22 @@ Todos.TodosRoute = Ember.Route.extend({
     return this.store.find('todo');
   }
 });
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+
